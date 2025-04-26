52746e5c62a2140c602f2dfa6b482204b6c2deea
David Perjans <perjansdavid@gmail.com>
2025-04-26 10:39:02 +0200

    Added Generic Repository with CRUD to minimize a lot of code

---
+++ Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<OperationResult<IEnumerable<T>>> GetAllAsync();
+        Task<OperationResult<T>> GetByIdAsync(string id);
+        Task<OperationResult<T>> AddAsync(T entity);
+        Task<OperationResult<T>> UpdateAsync(T entity);
+        Task<OperationResult<bool>> DeleteAsync(string id);
+
+    }
+}
---
+++ Domain/Models/OperationResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class OperationResult<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string? ErrorMessage { get; set; }
+        public T? Data { get; set; }
+
+        public static OperationResult<T> Success(T data) =>
+            new() { IsSuccess = true, Data = data };
+
+        public static OperationResult<T> Failure(string errorMessage) =>
+            new() { IsSuccess = false, ErrorMessage = errorMessage };
+    }
+}
