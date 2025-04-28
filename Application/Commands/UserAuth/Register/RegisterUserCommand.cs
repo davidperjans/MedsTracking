54a0fa0b10b407fe417c64eccd01bff9767f1053
David Perjans <perjansdavid@gmail.com>
2025-04-28 12:40:57 +0200

    Added commands & commandhandlers for logging in a user and also register a user

---
+++ Application/Commands/UserAuth/Register/RegisterUserCommand.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.UserAuth.Register
+{
+    public class RegisterUserCommand : IRequest<OperationResult<string>>
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
